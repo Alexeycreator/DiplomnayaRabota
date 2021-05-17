33e6e1650892baf8a043148227dd3495086fae7c
Alexeycreator <rushev.alexey@gmail.com>
2021-05-17 10:37:35 +0300

    new project

--- my-react/src/components/calendar/GoogleCalendar.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class GoogleCalendar extends Component {
-  render() {
-    return (
-      <div className="mt-5 d-flex justify-content-left">GoogleCalendar</div>
-    );
-  }
-}
--- my-react/src/components/calendar/OutlookCalendar.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class OutlookCalendar extends Component {
-  render() {
-    return (
-      <div className="mt-5 d-flex justify-content-left">OutlookCalendar</div>
-    );
-  }
-}
